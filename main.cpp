4c01ca55f44c3f1ae22f2558f72cb3d63b146ac1
RikuNav <riku@LAPTOP-KKRDV7KA.localdomain>
2022-11-17 10:50:52 -0600

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -2,16 +2,40 @@
 //
 // File: main.cpp
 // Author: Alain Vicencio A01620758
-// Fernnado Matute A00833375
-// Ricardo Navarro A01708825
+// 		   Fernnado Matute A00833375
+// 		   Ricardo Navarro A01708825
 // 
 // Date: 18/11/2022
 //
 // =================================================================
 #include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
+
+	ifstream input;
+    fstream mySolution;
+
+	if (argc != 3){
+    	cout<< "Error"<<endl;
+    	return -1;
+  	}
+
+ 	input.open(argv[1]);
+ 	 if (!input.is_open()){
+		cout << "Error al abrir el archivo\n";
+		return -1;
+  	}
+
+	mySolution.open(argv[2]);
+
 	return 0;
 }
